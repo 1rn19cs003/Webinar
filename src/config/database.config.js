a032380d71f7bab4bd97d86d28eac1d92eef9ce7
Abhishek Jaiswal <abhigrmr@gmail.com>
2022-12-03 10:41:12 +0530

    shanu

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,22 +1,27 @@
 // ************************************abhishek************************************
 // this page is created to connect the databse 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 // mysql connection
 // connection requires hostname,user (you can change based on your local databse)
 // password (again depend on your MySql workbench or configuration)
 // at the end we need to specify the databse in which you are working on
-const dbConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'webdev'
-});
+// const dbConnection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'webdev'
+// });
 
+const Sequelize = require("sequelize");
+module.exports = new Sequelize("demo", "root", "", {
+    host: "localhost",
+    dialect: "mysql"
+})
 
 // to hadle the errors
-dbConnection.connect(function (error) {
-    if (error) throw error;
-    console.log('Database Connection is established !');
-})
+// dbConnection.connect(function (error) {
+//     if (error) throw error;
+//     console.log('Database Connection is established !');
+// })
 // to export the database information
-module.exports = dbConnection;
+// module.exports = dbConnection;
